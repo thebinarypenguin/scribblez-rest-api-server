d1ebfca8200b392ea761a31f87b6e857bb7d4bbc
Ethan Zimmerman <thebinarypenguin@gmail.com>
2017-03-02 12:55:42 -0500

    Add tests for groupName schema

---
+++ test/schemas/group/groupName.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const Code    = require('code');
+const Hapi    = require('hapi');
+const Joi     = require('joi');
+const Lab     = require('lab');
+const config  = require('../../../src/config');
+const schemas = require('../../../src/schemas');
+
+const lab = exports.lab = Lab.script();
+
+lab.experiment('groupName schema', () => {
+
+  const server = new Hapi.Server();
+  
+  lab.before((done) => {
+
+    server.connection({
+      host: config.hapi.host,
+      port: config.hapi.port,
+    });
+
+    server.register([
+      schemas,
+    ], (err) => {
+
+      if (err) { throw err; }
+
+      server.initialize((err) => {
+
+        if (err) { throw err; }
+
+        done();
+      });
+    });
+  });
+
+  lab.test('must be a string', (done) => {
+
+    const good = 'Just John';
+
+    const bad = 999;
+
+    Code.expect(Joi.assert.bind(this, good, server.plugins.schemas.groupName)).to.not.throw();
+    Code.expect(Joi.assert.bind(this, bad, server.plugins.schemas.groupName)).to.throw();
+
+    done();
+  });
+
+  lab.test('must be at least 1 character long', (done) => {
+
+    const good = 'Just John';
+
+    const bad = '';
+
+    Code.expect(Joi.assert.bind(this, good, server.plugins.schemas.groupName)).to.not.throw();
+    Code.expect(Joi.assert.bind(this, bad, server.plugins.schemas.groupName)).to.throw();
+
+    done();
+  });
+
+  lab.test('must be at most 80 characters long', (done) => {
+
+    const good = 'Just John';
+
+    const bad = 'x'.repeat(81);
+
+    Code.expect(Joi.assert.bind(this, good, server.plugins.schemas.groupName)).to.not.throw();
+    Code.expect(Joi.assert.bind(this, bad, server.plugins.schemas.groupName)).to.throw();
+
+    done();
+  });
+});
