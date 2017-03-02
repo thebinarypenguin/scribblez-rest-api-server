82af0a253bf37373187d4567b794e1ec8df63743
Ethan Zimmerman <thebinarypenguin@gmail.com>
2017-03-02 12:55:20 -0500

    Add tests for noteID schema

---
+++ test/schemas/note/noteID.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Code    = require('code');
+const Hapi    = require('hapi');
+const Joi     = require('joi');
+const Lab     = require('lab');
+const config  = require('../../../src/config');
+const schemas = require('../../../src/schemas');
+
+const lab = exports.lab = Lab.script();
+
+lab.experiment('noteID schema', () => {
+
+  const server = new Hapi.Server();
+  
+  lab.before((done) => {
+
+    server.connection({
+      host: config.hapi.host,
+      port: config.hapi.port,
+    });
+
+    server.register([
+      schemas,
+    ], (err) => {
+
+      if (err) { throw err; }
+
+      server.initialize((err) => {
+
+        if (err) { throw err; }
+
+        done();
+      });
+    });
+  });
+
+  lab.test('must be an integer', (done) => {
+
+    const good = 42;
+
+    const bad = '42';
+
+    Code.expect(Joi.assert.bind(this, good, server.plugins.schemas.noteID)).to.not.throw();
+    Code.expect(Joi.assert.bind(this, bad, server.plugins.schemas.noteID)).to.throw();
+
+    done();
+  });
+});
