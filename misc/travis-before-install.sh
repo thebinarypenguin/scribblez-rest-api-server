3bbfb900e278dad76450813fa04bb28ff3fe40d0
Ethan Zimmerman <thebinarypenguin@gmail.com>
2017-03-27 18:01:48 -0400

    Add Travis CI config

---
+++ misc/travis-before-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SQL="CREATE DATABASE scribblez; "
+SQL+="CREATE ROLE scribblez WITH LOGIN; "
+SQL+="GRANT ALL PRIVILEGES ON DATABASE scribblez TO scribblez; "
+
+psql -c $SQL -U postgres
+
+git clone https://github.com/thebinarypenguin/scribblez-database.git
+
+cd scribblez-database
+
+npm install
+
+cp knexfile.example.js knexfile.js
+
+./node_modules/knex/bin/cli.js migrate:latest
+
+./node_modules/knex/bin/cli.js seed:run
