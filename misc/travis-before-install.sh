280f83e0d2e0c84b0cb402ab926165f877c7a144
Ethan Zimmerman <thebinarypenguin@gmail.com>
2017-03-27 18:13:53 -0400

    Tweak before install script, again

--- misc/travis-before-install.sh
+++ misc/travis-before-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SQL="CREATE DATABASE scribblez; "
-SQL+="CREATE ROLE scribblez WITH LOGIN; "
-SQL+="GRANT ALL PRIVILEGES ON DATABASE scribblez TO scribblez; "
+psql -c 'CREATE DATABASE scribblez;' -U postgres
 
-psql -c "$SQL" -U postgres
+psql -c 'CREATE ROLE scribblez WITH LOGIN;' -U postgres
+
+psql -c 'GRANT ALL PRIVILEGES ON DATABASE scribblez TO scribblez;' -U postgres
 
 git clone https://github.com/thebinarypenguin/scribblez-database.git
 
