cbda89feabea8ccb253a0d9ed1d01625d6611a36
Ethan Zimmerman <thebinarypenguin@gmail.com>
2017-03-27 20:18:18 -0400

    More GCC changes

--- misc/travis-before-install.sh
+++ misc/travis-before-install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-
-sudo apt-get update -q
-
-sudo apt-get install g++-4.8 -y
-
 psql -c 'CREATE DATABASE scribblez;' -U postgres
 
 psql -c 'CREATE ROLE scribblez WITH LOGIN;' -U postgres
@@ -21,5 +15,3 @@ npm install
 cp knexfile.example.js knexfile.js
 
 ./node_modules/knex/bin/cli.js migrate:latest
-
-./node_modules/knex/bin/cli.js seed:run
