a48fada4ef0723fc8c1887d8c3181157c98c2e8c
Ethan Zimmerman <thebinarypenguin@gmail.com>
2017-03-31 22:53:59 -0400

    Quick attempt at issue #1

--- misc/travis-script.sh
+++ misc/travis-script.sh
@@ -6,6 +6,4 @@
 
 npm install -g codecov
 
-./node_modules/lab/bin/lab -cv -I __core-js_shared__ -m 10000 -r console -o stdout -r lcov -o lcov.info
-
-codecov
+./node_modules/lab/bin/lab -cv -I __core-js_shared__ -m 10000 -r console -o stdout -r lcov -o lcov.info && codecov
